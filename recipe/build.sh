87a90de47ec96e585351c9f17464ddd340e6ad23
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2023-08-25 20:50:13 -0700

    Add support for osx-arm64 (#6)

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,11 @@
 set -ex
 
 export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
+    export COMPILER_WORKS_EXITCODE=0
+    export COMPILER_WORKS_EXITCODE__TRYRUN_OUTPUT=""
+fi
+
 export KALDI_ROOT=$PREFIX
 $PYTHON -m pip install . --no-deps -vv
