39176165fc1df3575237abe1c28b213b42c1afa0
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2024-03-18 13:36:48 -0700

    Update test script

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,7 +1,9 @@
 import os
+print("CF_CUDA_ENABLED:", os.environ.get('CF_CUDA_ENABLED', "False"))
+cuda_compiler_version = os.environ.get('cuda_compiler_version', "None")
+print("cuda_compiler_version:", cuda_compiler_version)
 
-test_cuda = os.environ.get('CF_CUDA_ENABLED', "False") == "True"
-
+test_cuda = cuda_compiler_version != "None"
 if test_cuda:
     print("TESTING CUDA")
 else:
@@ -9,7 +11,6 @@ else:
 
 
 if __name__ == '__main__':
-
     if not test_cuda:
         import _kalpy
         import kalpy
